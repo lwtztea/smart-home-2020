c8e300d00ab3de14469757b9e149eb51aa6a6228
Natalia Luneva <61375514+lwtztea@users.noreply.github.com>
2020-03-04 17:07:43 +0300

    Update SensorEvent.java

--- src/main/java/ru/sbt/mipt/oop/SensorEvent.java
+++ src/main/java/ru/sbt/mipt/oop/SensorEvent.java
@@ -17,6 +17,14 @@ public class SensorEvent {
         return objectId;
     }
 
+    public static SensorEvent getNextRandomSensorEvent() {
+        // pretend like we're getting the events from physical world, but here we're going to just generate some random events
+        if (Math.random() < 0.05) return null; // null means end of event stream
+        SensorEventType sensorEventType = SensorEventType.values()[(int) (4 * Math.random())];
+        String objectId = "" + ((int) (10 * Math.random()));
+        return new SensorEvent(sensorEventType, objectId);
+    }
+    
     @Override
     public String toString() {
         return "SensorEvent{" +
