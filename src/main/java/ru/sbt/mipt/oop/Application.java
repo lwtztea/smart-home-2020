1e7ac2d7090962fc6f44edf078666d212bf871f7
Natalia Luneva <61375514+lwtztea@users.noreply.github.com>
2020-03-07 15:12:14 +0300

    Update Application.java

--- src/main/java/ru/sbt/mipt/oop/Application.java
+++ src/main/java/ru/sbt/mipt/oop/Application.java
@@ -1,15 +1,22 @@
 package ru.sbt.mipt.oop;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
 
-    public static void main(String... args) {
+    public static void main(String... args) throws IOException {
 
-        SmartHome smartHome = SmartHomeStateReader.readJson("smart-home-1.js");
-        SensorEvent event = SensorEvent.getNextRandomSensorEvent();
+        SmartHome smartHome = new SmartHomeStateReader().readJson("smart-home-1.js");
+        SensorEvent event = new SensorEventGenerator().getRandomSensorEvent();
+        List<EventHandler> handlers = Arrays.asList(new DoorEventHandler(smartHome), new LightEventHandler(smartHome));
         while (event != null) {
             System.out.println("Got event: " + event);
-            EventHandler.handleSmartHomeEvent(smartHome, event);
-            event = SensorEvent.getNextRandomSensorEvent();
+            for (EventHandler handler: handlers) {
+                handler.handleSmartHomeEvent(event);
+            }
+            event = new SensorEventGenerator().getRandomSensorEvent();
         }
     }
 }
