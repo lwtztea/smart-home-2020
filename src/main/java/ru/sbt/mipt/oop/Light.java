3d5921eb390b07a1eaa8bd978a4d3ee6417b81e0
Natalia Luneva <61375514+lwtztea@users.noreply.github.com>
2020-03-11 23:38:22 +0300

    Update Light.java

--- src/main/java/ru/sbt/mipt/oop/Light.java
+++ src/main/java/ru/sbt/mipt/oop/Light.java
@@ -1,16 +1,12 @@
 package ru.sbt.mipt.oop;
 
 public class Light {
-    private boolean isOn;
     private final String id;
+    private boolean isOn;
 
     public Light(String id, boolean isOn) {
         this.id = id;
         this.isOn = isOn;
-    }
-
-    public boolean isOn() {
-        return isOn;
     }
 
     public String getId() {
