cc81ad9314300b1a34bc82a0af56695c5ff964c6
Natalia Luneva <61375514+lwtztea@users.noreply.github.com>
2020-03-11 23:33:59 +0300

    Create CommandSender.java

---
+++ src/main/java/ru/sbt/mipt/oop/CommandSender.java
@@ -0,0 +1,8 @@
+package ru.sbt.mipt.oop;
+
+public class CommandSender {
+
+    static void sendCommand(SensorCommand command) {
+        System.out.println("Pretend we're sending command " + command);
+    }
+}
