717584c02933e91993fad02eeb93c581ce51a668
Natalia Luneva <61375514+lwtztea@users.noreply.github.com>
2020-03-12 11:45:58 +0300

    Create TestSmartHomeCreator.java

---
+++ src/main/test/ru/sbt/mipt/oop/TestSmartHomeCreator.java
@@ -0,0 +1,28 @@
+package ru.sbt.mipt.oop;
+
+import java.util.Arrays;
+
+public class TestSmartHomeCreator {
+
+    private SmartHome smartHome;
+
+    TestSmartHomeCreator() {
+        Room kitchen = new Room(Arrays.asList(new Light("1", false), new Light("2", true)),
+                Arrays.asList(new Door(false, "1")),
+                "kitchen");
+        Room bathroom = new Room(Arrays.asList(new Light("3", true)),
+                Arrays.asList(new Door(false, "2")),
+                "bathroom");
+        Room bedroom = new Room(Arrays.asList(new Light("4", false), new Light("5", false), new Light("6", false)),
+                Arrays.asList(new Door(true, "3")),
+                "bedroom");
+        Room hall = new Room(Arrays.asList(new Light("7", false), new Light("8", false), new Light("9", false)),
+                Arrays.asList(new Door(false, "4")),
+                "hall");
+        this.smartHome = new SmartHome(Arrays.asList(kitchen, bathroom, bedroom, hall));
+    }
+
+    public SmartHome getSmartHome() {
+        return this.smartHome;
+    }
+}
