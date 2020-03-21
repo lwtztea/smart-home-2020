59756d96a5737953897879a6bfbd827de9c89ed8
Natalia Luneva <61375514+lwtztea@users.noreply.github.com>
2020-03-21 15:56:23 +0300

    Исправления с первого дз

---
+++ src/main/java/ru/sbt/mipt/oop/RandomEventGenerator.java
@@ -0,0 +1,12 @@
+package ru.sbt.mipt.oop;
+
+public class RandomEventGenerator implements SensorEventGenerator {
+
+    public SensorEvent getEvent() {
+
+        if (Math.random() < 0.05) return null; // null means end of event stream
+        SensorEventType sensorEventType = SensorEventType.values()[(int) (4 * Math.random())];
+        String objectId = "" + ((int) (10 * Math.random()));
+        return new SensorEvent(sensorEventType, objectId);
+    }
+}
