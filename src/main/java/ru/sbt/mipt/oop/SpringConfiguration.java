7ac6fdc070d3144054cbf287f6f36168e8bc2ecc
Natalia Luneva <61375514+lwtztea@users.noreply.github.com>
2020-04-01 16:41:48 +0300

    Update SpringConfiguration.java

--- src/main/java/ru/sbt/mipt/oop/SpringConfiguration.java
+++ src/main/java/ru/sbt/mipt/oop/SpringConfiguration.java
@@ -2,14 +2,57 @@ package ru.sbt.mipt.oop;
 
 import com.coolcompany.smarthome.events.SensorEventsManager;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Configuration
 public class SpringConfiguration {
 
     @Bean
-    public SensorEventsManager sensorEventsManager(EventHandler handler) {
+    SmartHome smartHome() {
+        return new SmartHomeStateReader().readJson("smart-home-1.js");
+    }
+
+    @Bean
+    EventHandler alarmEventHandler(SmartHome smartHome) {
+        return new AlarmEventHandler(smartHome);
+    }
+
+    @Bean
+    EventHandler lightEventHandler(SmartHome smartHome) {
+        return new LightEventHandler(smartHome);
+    }
+
+    @Bean
+    EventHandler doorEventHandler(SmartHome smartHome) {
+        return new DoorEventHandler(smartHome);
+    }
+
+    @Bean
+    EventHandler hallDoorEventHandler(SmartHome smartHome) {
+        return new HallDoorEventHandler(smartHome);
+    }
+
+    @Bean
+    Map<String, SensorEventType> adaptedEventType() {
+        return new HashMap<String, SensorEventType>() {
+            {
+                put("LightIsOn", SensorEventType.LIGHT_ON);
+                put("LightIsOff", SensorEventType.LIGHT_OFF);
+                put("DoorIsOpen", SensorEventType.DOOR_OPEN);
+                put("DoorIsClosed", SensorEventType.DOOR_CLOSED);
+            }
+        };
+    }
+
+    @Bean
+    public SensorEventsManager sensorEventsManager(List<EventHandler> handlers, Map<String, SensorEventType> adaptedEventType) {
 
         SensorEventsManager sensorEventsManager = new SensorEventsManager();
-        sensorEventsManager.registerEventHandler(new EventHandlerAdapter(handler));
+        sensorEventsManager.registerEventHandler(new EventHandlerAdapter(handlers, adaptedEventType));
         return sensorEventsManager;
     }
 }
