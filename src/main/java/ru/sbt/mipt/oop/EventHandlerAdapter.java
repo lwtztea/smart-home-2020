7e08b91c2d5bf8834513065b3683df0c1b3d38ed
Natalia Luneva <61375514+lwtztea@users.noreply.github.com>
2020-04-01 16:33:14 +0300

    Update EventHandlerAdapter.java

--- src/main/java/ru/sbt/mipt/oop/EventHandlerAdapter.java
+++ src/main/java/ru/sbt/mipt/oop/EventHandlerAdapter.java
@@ -3,15 +3,22 @@ package ru.sbt.mipt.oop;
 import com.coolcompany.smarthome.events.CCSensorEvent;
 import com.coolcompany.smarthome.events.EventHandler;
 
+import java.util.List;
+import java.util.Map;
+
 public class EventHandlerAdapter implements EventHandler {
 
-    private final ru.sbt.mipt.oop.EventHandler eventHandler;
+    private final List<ru.sbt.mipt.oop.handlers.EventHandler> handlers;
+    private final Map<String, SensorEventType> adaptedEventType;
 
-    public EventHandlerAdapter(ru.sbt.mipt.oop.EventHandler eventHandler) {
-        this.eventHandler = eventHandler;
+    public EventHandlerAdapter(List<ru.sbt.mipt.oop.handlers.EventHandler> handlers, Map<String, SensorEventType> adaptedEventType) {
+        this.handlers = handlers;
+        this.adaptedEventType = adaptedEventType;
     }
 
     public void handleEvent(CCSensorEvent event) {
-        eventHandler.handleEvent(new SensorEventAdapter(event));
+        for (ru.sbt.mipt.oop.handlers.EventHandler handler : handlers) {
+            handler.handleEvent(new SensorEvent(adaptedEventType.get(event.getEventType()), event.getObjectId()));
+        }
     }
 }
